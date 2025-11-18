120445a0c92f0cc6dfe32bd1a6f926e4abbad3b0
kimhyeone <whho_key@naver.com>
2025-11-18 21:29:38 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+	double div;
+
+	scanf("%d %d", &A, &B);
+
+	div = (double)A / (double)B;
+	printf("%.9lf", div);
+
+	return 0;
+}
