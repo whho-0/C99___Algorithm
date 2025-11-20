5ddd3fa96858a934b33d0b5ea96a894b83a5037b
kimhyeone <whho_key@naver.com>
2025-11-20 22:05:28 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// 1330 - 두 수 비교하기
+int main(void)
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+	if(A>B)
+		printf(">");
+
+	if(A<B)
+		printf("<");
+
+	if(A==B)
+		printf("==");
+	
+	return 0;
+}
