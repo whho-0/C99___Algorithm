2150e15e76a341115415eb86fe67cb61a9dafd59
kimhyeone <whho_key@naver.com>
2025-11-20 11:40:38 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// 특수문자 출력 개념 이용
+int main(void) 
+{
+	printf("\\    /\\\n"); // 역슬래시 마지막부터 시작해서 띄어쓰기 세야되나?
+	printf(" )  ( ')\n");   // O 마지막 역슬래시부터가 출력 위치임.
+	printf("(  /  )\n");   
+	printf(" \\(__)|\n");
+	return 0;
+} 
