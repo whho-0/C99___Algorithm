27df6ecad0459e52edbf3a36e1d331933c900cc8
kimhyeone <whho_key@naver.com>
2025-11-21 11:57:28 +0900

    error cause

--- 백준/Bronze/2753. 윤년/윤년.c
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -5,11 +5,12 @@ int main(void)
 	int leapyear;
 	scanf("%d", &leapyear);
 
-	if(leapyear%4==0 && (leapyear%100!=0 || leapyear%400==0))
+	if(leapyear%4==0 && (leapyear%100!=0 || leapyear%400==0)) // 괄호 위치 중요
 		printf("1");
 	
 	else
 		printf("0");
         
 	return 0;
+
 }
