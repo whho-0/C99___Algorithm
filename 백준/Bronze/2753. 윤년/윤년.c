d35c6a896bf5c869e3991432dff8e9e55c95097d
kimhyeone <whho_key@naver.com>
2025-11-21 11:55:21 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// 2753 - 윤년
+int main(void)
+{
+	int leapyear;
+	scanf("%d", &leapyear);
+
+	if(leapyear%4==0 && (leapyear%100!=0 || leapyear%400==0))
+		printf("1");
+	
+	else
+		printf("0");
+        
+	return 0;
+}
