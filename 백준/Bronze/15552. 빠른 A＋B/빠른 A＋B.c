f14d4c77b71a584d68f8c64de6f015661bba9757
kimhyeone <whho_key@naver.com>
2025-11-29 16:55:57 +0900

    C에서의 입출력 실행 속도 레퍼런스 추가
    Added comments explaining the efficiency of scanf and printf for input/output speed.

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -1,6 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-// 15552 - 빠른 A+B
+/* 15552 - 빠른 A+B
+
+scanf와 printf는 다른 언어에 비해 '입출력 실행 속도'가 빠른 편에 속하기에 시간 단축 명령어 고려 X
+입력 속도 비교 레퍼런스 : https://www.acmicpc.net/blog/view/56
+
+*/
+
 int main(void)
 {
 	int T, A, B;
@@ -13,4 +19,5 @@ int main(void)
 	}
 
 	return 0;
+
 }
